713a8e6d2fcf0a1e7db82924fb74b9e08f126913
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-06-12 17:52:27 +0400

    update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import logging
 # ◇─────────────────────────────────────────────────────────────────────────────────────◇
 
 # TikTok Downloader API
-API = 'https://api.reiyuura.me/api/dl/tiktok?url='
+API = f"https://tiktok-info.p.rapidapi.com/dl/"
 
 # Your BOT Token
 BOT_TOKEN = os.getenv("BOT_TOKEN")
